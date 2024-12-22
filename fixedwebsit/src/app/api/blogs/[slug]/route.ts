6fa92e1f877e17eb569b2c0e19c5c2bc7cf81715
MekaSid <sidmekarao@gmail.com>
2024-12-21 20:54:15 -0800

    fix: vercel route.ts

--- fixedwebsit/src/app/api/blogs/[slug]/route.ts
+++ fixedwebsit/src/app/api/blogs/[slug]/route.ts
@@ -4,7 +4,12 @@ import { NextRequest, NextResponse } from "next/server";
 import connectDB from "@/database/db";
 import blogSchema from "@/database/blogSchema";
 
-export async function GET(req: NextRequest, context: any) { // Let TypeScript infer the type
+interface RouteContext {
+  params: { slug: string };
+  searchParams: URLSearchParams;
+}
+
+export async function GET(req: NextRequest, context: RouteContext) {
   const { slug } = context.params; // Access slug from params
   await connectDB(); // Connect to the database
 
