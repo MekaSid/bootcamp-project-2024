5f49601c866e928ffc6edc06f68ee42a4c170fe4
MekaSid <sidmekarao@gmail.com>
2024-12-21 20:42:32 -0800

    fix: vercel issue route.ts

--- fixedwebsit/src/app/api/blogs/[slug]/route.ts
+++ fixedwebsit/src/app/api/blogs/[slug]/route.ts
@@ -2,8 +2,8 @@ import { NextRequest, NextResponse } from "next/server";
 import connectDB from "@/database/db";
 import blogSchema from "@/database/blogSchema";
 
-export async function GET(req: NextRequest, context: { params: { slug: string } }) {
-  const { slug } = context.params; // Access slug from context.params
+export async function GET(req: NextRequest, { params }: { params: { slug: string } }) {
+  const { slug } = params; // Access slug directly from params
   await connectDB(); // Connect to the database
 
   try {
