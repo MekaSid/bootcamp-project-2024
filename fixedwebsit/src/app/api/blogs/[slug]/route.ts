08a4745cf32718cce7b4c8ea8b69deb63fdd6b04
MekaSid <sidmekarao@gmail.com>
2024-12-22 11:57:33 -0800

    fix: vercel route.ts

--- fixedwebsit/src/app/api/blogs/[slug]/route.ts
+++ fixedwebsit/src/app/api/blogs/[slug]/route.ts
@@ -1,24 +1,22 @@
+import { NextRequest, NextResponse } from "next/server";
+import connectDB from "../../../../database/db";
+import blogSchema from "../../../../database/blogSchema";
 
-import { NextRequest, NextResponse } from 'next/server'
-import connectDB from "@/database/db"
-import blogSchema from "@/database/blogSchema"
+export async function GET(
+  req: NextRequest,
+  {
+    params,
+  }: {
+    params: Promise<{ slug: string }>;
+  }
+) {
+  const slug = (await params).slug;
+  await connectDB(); // function from db.ts before
 
-type IParams = {
-		params: {
-			slug: string
-		}
-}
-
-// If { params } looks confusing, check the note below this code block
-export async function GET(req: NextRequest, { params }: IParams) {
-    await connectDB() // function from db.ts before
-		const { slug } = params // another destructure
-
-	   try {
-	        const blog = await blogSchema.findOne({ slug }).orFail()
-	        return NextResponse.json(blog)
-	    } catch (err) {
-          console.log(err)
-	        return NextResponse.json('Blog not found.', { status: 404 })
-	    }
+  try {
+    const blog = await blogSchema.findOne({ slug }).orFail();
+    return NextResponse.json(blog);
+  } catch (err) {
+    return NextResponse.json("Blog not found.", { status: 404 });
+  }
 }
